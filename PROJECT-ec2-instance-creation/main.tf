fac2ace94709dcb1f7175e8c9edec653add6b1bb
marineking5248 <liuxiao5248@hotmail.com>
2026-07-07 21:03:58 +0200

    feature/play-ground-practises

--- PROJECT-ec2-instance-creation/main.tf
+++ PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0b6d9d3d33ba97d99"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id     = "subnet-7e1f0e71"  # Specify your subnet ID
+    key_name    = "terraform_playground_1"  # Specify your key pair name
 }
+
+# for more information about providers, visit https://registry.terraform.io/
+# video tutorial: https://www.youtube.com/watch?v=fgp-t5SqQmM&list=PLdpzxOOAlwvI0O4PeKVV1-yJoX2AqIWuf
